bfc89d428dc1dc7421c0a479f18f19a8013449ca
Jon Rice <revyolution1120@gmail.com>
2019-06-15 15:20:41 -0700

    Working on play game

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -9,6 +9,24 @@ namespace Lab04_TicTacToe
             PlayGame();
         }
 
+        static void PlayGame() 
+        {
+            do 
+            {
+                Console.Clear();
+                Console.WriteLine("Let's Play Tic-Tac-Toe");
+                Console.WriteLine("Player 1: What is your name?");
+                Player playerOne = new Player()
+                {
+                    Name = Console.ReadLine(),
+                    IsTurn = true, 
+                    Marker = "X"
+                };
+
+            }
+
+        }
+
         // TODO: Setup your game. Create a new method that creates your players and instantiates the game class. 
         // You are requesting a Winner to be returned, Determine who the winner is output the celebratory message to the correct player. If it's a draw, tell them. 
     }
