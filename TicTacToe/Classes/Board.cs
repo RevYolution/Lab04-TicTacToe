09972284651a49d01bc26e0a7073b75c88b1f844
Jon Rice <revyolution1120@gmail.com>
2019-06-15 11:47:32 -0700

    Game Board done.

--- TicTacToe/Classes/Board.cs
+++ TicTacToe/Classes/Board.cs
@@ -16,11 +16,19 @@ namespace Lab04_TicTacToe.Classes
 			{"7", "8", "9"},
 		};
 
-
+        /// <summary>
+        /// Displays game board to the console.
+        /// </summary>
 		public void DisplayBoard()
 		{
+			Console.Clear();
+            Console.WriteLine();
+            for (int i = 0; i < 3; i++)
+			{
+                Console.WriteLine($"{GameBoard[i, 0]} | {GameBoard[i, 1]} | {GameBoard[i, 2]}");
+                Console.WriteLine($"{(i < 2 ? "---|---|--- " : "")}");
+			}
 			
-			//TODO: Output the board to the console
 		
 		}
 	}
